cb2a2f6554ab72eafc13119a72572caa8ba181cb
Abhilashreddy <kasireddyabhilashredy@gmail.com>
2022-08-03 11:14:03 +0530

    changes added

--- myAngularApp/src/app/app-routing.module.ts
+++ myAngularApp/src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth.guard';
 import { DashboardLayoutComponent } from './dashboard/dashboard-layout/dashboard-layout.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-   { path: '', component: LoginComponent },
+   { path: '', component: RegisterComponent },
    { path: 'dashboard', component: DashboardLayoutComponent,canActivate: [AuthGuard]  }
 ];
 
