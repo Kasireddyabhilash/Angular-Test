1823dfc9dce2b6259ae2b79f22a75ab397341299
Abhilashreddy <kasireddyabhilashredy@gmail.com>
2022-08-03 11:10:43 +0530

    changes added

--- myAngularApp/src/app/app-routing.module.ts
+++ myAngularApp/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { DashboardLayoutComponent } from './dashboard/dashboard-layout/dashboard-layout.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  // { path: '', component: LoginComponent }
+   { path: '', component: LoginComponent },
+   { path: 'dashboard', component: DashboardLayoutComponent,canActivate: [AuthGuard]  }
 ];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- myAngularApp/src/app/app.module.ts
+++ myAngularApp/src/app/app.module.ts
@@ -5,11 +5,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './dashboard/header/header.component';
+import { FooterComponent } from './dashboard/footer/footer.component';
+import { SidebarComponent } from './dashboard/sidebar/sidebar.component';
+import { DashboardLayoutComponent } from './dashboard/dashboard-layout/dashboard-layout.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    DashboardLayoutComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
